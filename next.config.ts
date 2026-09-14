b8aa5648fe7fbe0b94bb90dcb88550ce1bba0b24
Sérgio Timoteo <sergiogabrieltimoteo@gmail.com>
2026-09-14 12:46:35 -0300

    chore(config): allow ngrok for dev HMR and add prisma postinstall

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: ['unhairy-laquanda-indefinitely.ngrok-free.dev'],
 };
 
 export default nextConfig;
